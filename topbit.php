a22408b911aa5566de6e7c801359de87555fc2ed
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-07-27 13:16:15 +1200

    Error fixed in 'topbit.php'
    I typed in the 'DB_HOST' and typed 'Db_HOST' instead which then caused an error

--- topbit.php
+++ topbit.php
@@ -8,7 +8,7 @@
     include("config.php");
     
     // Connect to database
-    $dbconnect=mysqli_connect(Db_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
+    $dbconnect=mysqli_connect(DB_HOST,DB_USERNAME,DB_PASSWORD,DB_NAME);
     
     if(mysqli_connect_errno()) {
         echo "Connection failed:".mysqli_connect_error();
