1cdb5ebe2c29ddd961cf83e21da6890c1a76dd45
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-07-29 10:48:11 +1200

    Minor error fixed in DevName

--- showall.php
+++ showall.php
@@ -3,7 +3,7 @@
     $find_sql = "SELECT *
     FROM `00_L2_games`
     JOIN 00_L2_games_genre ON (00_L2_games.GenreID = 00_L2_games_genre.GenreID)
-    JOIN 00_L2_games_developer ON (00_L2_games.DeveloperID = 00_L2_games_genre.DeveloperID)
+    JOIN 00_L2_games_developer ON (00_L2_games.DeveloperID = 00_L2_games_developer.DeveloperID)
     
     ";
     $find_query = mysqli_query($dbconnect, $find_sql);
