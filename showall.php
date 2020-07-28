489a76575de6b10baf533de40b8a2571337189e1
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-07-29 10:27:48 +1200

    Start of showall.php file setup

--- showall.php
+++ showall.php
@@ -1,12 +1,54 @@
-<?php include("topbit.php")?>
+<?php include("topbit.php");
+
+    $find_sql = "SELECT *
+    FROM `00_L2_games`";
+    $find_query = sysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+    
+
+?>
 
         <div class="box main">
             <h2>All Results</h2>
             
             
-            <p>
-               Results will go here...
-            </p>
+            <?php
+            
+            if($count < 1) {
+              
+                ?>
+            <div class="error">
+            
+            Sorry! There are no results that match your search.
+            Please use the search box in the side bar to try again.
+                
+            </div> <!-- end error -->
+            
+            
+            <?php    
+            } // end no results if
+            else {
+                do
+                {
+                    ?>
+            <!-- Results fo here -->
+            <div class="results">
+                You have results!
+            </div> <!-- / results -->
+            
+            
+            <?php
+                } // end results 'do'
+                
+                while
+                    ($find_rs=mysqli_fetch_assoc($find_query));
+                
+                
+            } // end else
+            
+            
+            ?>
             
 
             
