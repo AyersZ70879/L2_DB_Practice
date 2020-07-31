83cb883c183536c41e3ab0c374ff86e747090426
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-07-31 14:41:03 +1200

    reimplamented dev genre age and description

--- showall.php
+++ showall.php
@@ -124,8 +124,18 @@
                         
                     } // end price else (display costs)
                 ?>
+            
+            <p>
+                <!-- Developer, Genre and Age... -->
+                <b>Developer:</b> <?php echo $find_rs['DevName']?> <br />
+                <b>Genre:</b> <?php echo $find_rs['Genre']?> <br />               
+                Suitable for ages <b><?php echo $find_rs['Age']?></b> and up
                 
+            </p>
                 
+            <p>
+                <i><?php echo $find_rs['Description']; ?></i>
+            </p>
                 
             </div> <!-- / results -->
             
