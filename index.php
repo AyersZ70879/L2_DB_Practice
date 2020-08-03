c2a883c0a8d31c435a7309038834997d40094649
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-08-04 11:40:46 +1200

    Acknowledgements and Sources added

--- index.php
+++ index.php
@@ -6,8 +6,16 @@
             
             <p>
                This database includes some of the most popular apps
-                available for mobile devices. The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games">original dataset</a> can be
+                available for mobile devices. 
+                
+            </p>
+            <h3>Acknowledgements and Sources</h3>
+            
+            <p>
+                The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games">original dataset</a> can be
                 found at the preceding link.
+                The <a href="https://codepen.io/Bluetidepro/pen/GkpEa">percentage based star rating</a> code is at the preceding link.
+            
             </p>
             
 
