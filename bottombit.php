a5fc7b797e9e63c9be4707e049ab8578afc5377a
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-08-05 10:20:45 +1200

    Changed wording

--- bottombit.php
+++ bottombit.php
@@ -1,6 +1,6 @@
         <div class="box side">
            
-           <h2>Add and App | <a class="side" href="showall.php">Show All</a></h2>
+           <h2>Add an App | <a class="side" href="showall.php">Show All</a></h2>
            
            <form class="searchform" method="post" action="name_dev.php" enctype="multipart/form-data">
             
