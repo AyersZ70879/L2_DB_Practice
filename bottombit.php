8844ad700fbfde65fc955fbbb49120ca3a164519
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-08-05 10:32:33 +1200

    Advanced Search added

--- bottombit.php
+++ bottombit.php
@@ -16,6 +16,24 @@
                 
             </form>
             
+            <br />
+            <hr />
+            <br />
+            
+            <div class="advanced-frame">
+            
+            <h2>Advanced Search</h2>
+                
+            <form class="searchform" method="post" action="advanced.php" enctype="multipart/form-data">
+            
+                    <p>Form goes here...</p>
+                <input class="submit advanced-button" type="submit" name="advanced" value="Search &nbsp; &#xf002;" />
+                
+            </form>
+            
+            </div> <!-- / advanced frame -->    
+                
+                
         </div> <!-- / side bar -->
         
         <div class="box footer">
