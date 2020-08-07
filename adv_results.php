4d03f4221ea0b8cfc895a0577436bbbc2398bd98
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-08-07 14:25:14 +1200

    adv search part 5 finished

--- adv_results.php
+++ adv_results.php
@@ -4,8 +4,19 @@
     $developer = mysqli_real_escape_string($dbconnect, $_POST['dev_name']);
     $genre = mysqli_real_escape_string($dbconnect, $_POST['genre']);
     $cost = mysqli_real_escape_string($dbconnect, $_POST['cost']);
+    
+    // Cost code (to handle when cost is not specified...)
+    if ($cost=="") {
+        $cost_op = ">=";
+        $cost = 0;
+    }
+    else {
+        $cost_op = "<=";
+    }
+    
 
-    if (isset($POST['in_app'])) {
+    // In App Purchases
+    if (isset($_POST['in_app'])) {
         $in_app = 0;
     }
     
@@ -13,14 +24,34 @@
         $in_app = 1;
     }
 
+    // Ratings
+    $rating_more_less = mysqli_real_escape_string($dbconnect, $_POST['rate_more_less']);
+    $rating = mysqli_real_escape_string($dbconnect, $_POST['rating']);
+
+    if ($rating_more_less == "at least") {
+        $rate_op = ">=";
+    }
+
+    elseif($rating_more_less == "at most") {
+        $rate_op = "<=";
+    }
+
+    else {
+        $rate_op = "<=";
+        $rating = 0;
+        
+    } // end rating if / elseif / else
+
+
     $find_sql = "SELECT * FROM `00_L2_games`
     JOIN 00_L2_games_genre ON (00_L2_games.GenreID = 00_L2_games_genre.GenreID)
     JOIN 00_L2_games_developer ON (00_L2_games.DeveloperID = 00_L2_games_developer.DeveloperID)
     WHERE `Name` LIKE '%$app_name%'
     AND `DevName` LIKE '%$developer%'
     AND `Genre` LIKE '%$genre%'
-    AND `Price` <= '$cost'
+    AND `Price` $cost_op '$cost'
     AND (`In App` = $in_app OR `In App` = 0)
+    AND `User Rating` $rate_op $rating
     
     ";
     $find_query = mysqli_query($dbconnect, $find_sql);
