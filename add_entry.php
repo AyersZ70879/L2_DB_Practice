41af2f30a819767789181a81887177264b25fd95
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-09-01 11:49:50 +1200

    Setup form part 1 - (complete)

---
+++ add_entry.php
@@ -0,0 +1,15 @@
+<?php include("topbit.php")?>
+
+        <div class="box main">
+            <div class="add-entry">
+            <h2>Add An Entry</h2>
+            
+            <form method="post" enctype="multipart/form-data" 
+                  action="<?php echo htmlspecialchars($_SERVER[]); ?>">
+                
+            </form>
+  
+            </div> <!-- / add-entry -->
+        </div> <!-- / main -->
+        
+<?php include("bottombit.php")?>
