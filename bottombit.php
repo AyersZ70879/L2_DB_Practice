519d2fac4eb0e6e7df82e66f2725ab694cedfc4e
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-09-01 11:39:00 +1200

    Start of entry forum setup part 1

--- bottombit.php
+++ bottombit.php
@@ -1,6 +1,6 @@
         <div class="box side">
            
-           <h2>Add an App | <a class="side" href="showall.php">Show All</a></h2>
+           <h2><a href="addentry.php">Add an App</a> | <a class="side" href="showall.php">Show All</a></h2>
            
            <form class="searchform" method="post" action="name_dev.php" enctype="multipart/form-data">
             
@@ -67,7 +67,7 @@
                 </div> <!-- / cost label -->
                 
                 <div>
-                    <input class="adv" type="text" name="cost" size="40" value="" placeholder="$..."/>
+                    <input class="adv-cost" type="text" name="cost" value="" placeholder="$..."/>
                 </div> <!-- / input box -->
                 
             </div> <!-- / cost flexbox -->
