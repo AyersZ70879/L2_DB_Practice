e691382e2b511d6e2f55be6d872730c07cb6012d
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-09-01 11:57:56 +1200

    Part way through 'Entry form setup part 2'

--- add_entry.php
+++ add_entry.php
@@ -1,11 +1,57 @@
-<?php include("topbit.php")?>
+<?php include("topbit.php");
+
+// Initialise from variables
+
+$app_name = "";
+$subtitle = "";
+$url = "";
+$genreID = "";
+$dev_name = "";
+$age = "";
+$rating = "";
+$rate_count = "";
+$cost = "";
+$inapp = 1;
+$description = "";
+
+$has_errors = "no";
+
+?>
 
         <div class="box main">
             <div class="add-entry">
             <h2>Add An Entry</h2>
             
             <form method="post" enctype="multipart/form-data" 
-                  action="<?php echo htmlspecialchars($_SERVER[]); ?>">
+                  action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
+                
+                <!-- App Name (Required) -->
+                <input class="add-field" type="text" name="app_name" value="<?php echo $app_name; ?>" placeholder="App Name (required) ..." />
+                
+                <!-- Subtitle (optional) -->
+                
+                <!-- URL (required, must start http://) -->
+                
+                <!-- Genre dropdown (required) -->
+                
+                <!-- Developer Name (required) -->
+                
+                <!-- Age (set to 0 if left blank) -->
+                
+                <!-- Rating (Number between 0-5, 1 dp) -->
+                
+                <!-- # of ratings (integer more than 0) -->
+                
+                <!-- Cost (Decimal 2dp, must be more than 0) -->
+                
+                <!-- In App Purchase radio buttons -->
+                
+                <!-- Description text area -->
+                
+                <!-- Submit Button -->
+                <p>
+                    <input class="submit advanced-button" type="submit" value="Submit" />
+                </p>
                 
             </form>
   
