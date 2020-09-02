e9aa012e0530766447b74da17e7945d5b8ce419f
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-09-03 09:42:13 +1200

    partway through 'add developer' (0:57)

--- add_entry.php
+++ add_entry.php
@@ -16,13 +16,58 @@ $age = "";
 $rating = "";
 $rate_count = "";
 $cost = "";
-$inapp = 1;
+$in_app = 1;
 $description = "";
 
 $has_errors = "no";
 
 // Code below excutes when the form is submitted...
 if($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    // Get values from the form
+    $app_name = mysqli_real_escape_string($dbconnect, $_POST['app_name']);
+    $subtitle = mysqli_real_escape_string($dbconnect, $_POST['subtitle']);
+    $url = mysqli_real_escape_string($dbconnect, $_POST['url']);
+    
+    $genreID = mysqli_real_escape_string($dbconnect, $_POST['genre']);
+    
+    // if GenreID, is not blank, get genre so that genre box does not lose its value if there is an error
+    if ($genreID != "") {
+        $genreitem_sql = "SELECT * FROM `genre` WHERE `GenreID` = $genreID";
+        $genreitem_query=mysqli_query($dbconnect, $genreitem_sql);
+        $genreitem_rsd=mysqli_fetch_assoc($genreitem_query);
+        
+        $genre = $genreitem_rs['Genre'];
+        
+    } //end genreID if
+    
+    $dev_name = mysqli_real_escape_string($dbconnect, $_POST['dev_name']);
+    $age = mysqli_real_escape_string($dbconnect, $_POST['age']);
+    $rating = mysqli_real_escape_string($dbconnect, $_POST['rating']);
+    $rate_count = mysqli_real_escape_string($dbconnect, $_POST['count']);
+    $cost = mysqli_real_escape_string($dbconnect, $_POST['price']);
+    $in_app = mysqli_real_escape_string($dbconnect, $_POST['in_app']);
+    $description = mysqli_real_escape_string($dbconnect, $_POST['description']);
+    
+    // error checking will go here
+    
+    // if there are no errors
+    if ($has_errors == "no") {
+    
+    // Go to success page
+    
+    // get developer ID if it exists
+    $dev_sql ="";
+    $dev_query=mysqli_query($dbconnect, $dev_sql;)
+    $dev_rs=mysqli_fetch_assoc($dev_query);
+    $dev_count=mysqli_num_rows($dev_query);
+    
+    // ig developer not already in developer table, add them and get the 'new' developerID
+    
+    // Add entry to database
+    
+    }   // end of 'no errors' if
+    
     echo "You pushed the button";
     
 } // end of button submitted code
@@ -37,7 +82,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                   action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>">
                 
                 <!-- App Name (Required) -->
-                <input class="add-field" type="text" name="app_name" value="<?php echo $app_name; ?>" placeholder="App Name (required) ..." />
+                <input class="add-field" type="text" required name="app_name" value="<?php echo $app_name; ?>" placeholder="App Name (required) ..." />
                 
                 <br />
                 
@@ -47,13 +92,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 <br />
                 
                 <!-- URL (required, must start http://) -->
-                <input class="add-field" type="text" name="url" value="<?php echo $url; ?>" placeholder="URL (required) ..." />
+                <input class="add-field" type="text" required name="url" value="<?php echo $url; ?>" placeholder="URL (required) ..." />
                 
                 <br />
                 
                 <!-- Genre dropdown (required) -->
-                <select class="adv" name="genre">
+                <select class="adv" required name="genre">
+                    <!-- first / selected option -->
+                    
+                    <?php 
+                    if($genreID=="") {
+                        ?>
                     <option value="" selected>Genre (Choose something)...</option>
+                    
+                    <?php
+                        
+                    }
+                    
+                    else {
+                       ?> 
+                    <option value="<?php echo $genreID; ?>" selected><?php echo $genre; ?></option>
+                    <?php 
+                    }
+                    ?>
                     
                     <!-- get options from database -->
                     <?php
@@ -72,7 +133,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 <br />
                 
                 <!-- Developer Name (required) -->
-                <input class="add-field" type="text" name="dev_name" value="<?php echo $dev_name; ?>" placeholder="Developer Name (required) ..." />
+                <input class="add-field" type="text" required name="dev_name" value="<?php echo $dev_name; ?>" placeholder="Developer Name (required) ..." />
                 
                 <br />
                 
@@ -83,19 +144,19 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 
                 <!-- Rating (Number between 0-5, 1 dp) -->
                 <div>
-                    <input class="add-field" type="text" name="rating" value="<?php echo $rating; ?>" step="0.1" min="0" max="5" placeholder="Rating (0-5)" />
+                    <input class="add-field" type="text" required name="rating" value="<?php echo $rating; ?>" step="0.1" min="0" max="5" placeholder="Rating (0-5)" />
                 </div>
                 
                 <br />
                 
                 <!-- # of ratings (integer more than 0) -->
-                <input class="add-field" type="text" name="count" value="<?php echo $rate_count; ?>" placeholder="# of Ratings ..." />
+                <input class="add-field" type="text" required name="count" value="<?php echo $rate_count; ?>" placeholder="# of Ratings ..." />
                 
                 <br />
                 
                 
                 <!-- Cost (Decimal 2dp, must be more than 0) -->
-                <input class="add-field" type="text" name="price" value="<?php echo $cost; ?>" placeholder="Cost (number only) ..." />
+                <input class="add-field" type="text" required name="price" value="<?php echo $cost; ?>" placeholder="Cost (number only) ..." />
                 
                 <br /><br />
                 
@@ -105,8 +166,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                     <!-- defaults to 'yes' -->
                     <!-- NOTE: value in databse boolean, so 'no' become 0 and 'yes' becomes 1 -->
                     
+                    <?php
+                    if($in_app==1) {
+                    // Defult value, 'YES' is selected
+                        ?>
                     <input type="radio" name="in_app" value="1" checked="checked" />Yes
                     <input type="radio" name="in_app" value="0" />No
+                    
+                    <?php    
+                    } // end 'yes in_app' if
+                    
+                    else{
+                        ?>
+                    <input type="radio" name="in_app" value="1" />Yes
+                    <input type="radio" name="in_app" value="0" checked="checked" />
+                    
+                    <?php                    
+                    } // end 'in_app' else
+                    ?>
                 
                 </div>
                 
