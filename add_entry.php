acba40ee078d60d4813f08194504ff106736497a
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-09-10 09:48:23 +1200

    Error still present - Finished 'add entry' video however code doesn't work

--- add_entry.php
+++ add_entry.php
@@ -96,9 +96,11 @@ WHERE `DevName` LIKE '$dev_name'";
         $addentry_sql = "INSERT INTO `ayersz70879`.`00_L2_games` (`ID`, `Name`, `Subtitle`, `URL`, `GenreID`, `DeveloperID`, `Age`, `User Rating`, `Rating Count`, `Price`, `In App`, `Description`) VALUES (NULL, '$app_name', 'subtitle', 'url', '2', '3', '16', '5', '1234', '1.99', '1', 'description');";
         $addentry_query=mysqli_query($dbconnect,addentry_sql);
     
+        // INSERT INTO `ayersz70879`.`00_L2_games` (`ID`, `Name`, `Subtitle`, `URL`, `GenreID`, `DeveloperID`, `Age`, `User Rating`, `Rating Count`, `Price`, `In App`, `Description`) VALUES (NULL, '$app_name', '$subtitle', '$url', '$genreID', '$developerID', '$age', '$rating', '$rate_count', '$cost', '$in_app', '$description');
+        
     }   // end of 'no errors' if
     
-    echo "You pushed the button";
+    
     
 } // end of button submitted code
 
