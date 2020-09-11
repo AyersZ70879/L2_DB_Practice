e84250339fa924c4713709d4bed63ec11ca8018d
AyersZ70879 <68571244+AyersZ70879@users.noreply.github.com>
2020-09-11 14:49:04 +1200

    Minor error on success page - EoDW

--- add_success.php
+++ add_success.php
@@ -1,12 +1,30 @@
-<?php include("topbit.php")?>
+<?php include("topbit.php")
 
+    // retrieves information...
+    $ID = $_SESSION['ID'];
+
+    $find_sql = "SELECT * FROM `00_L2_games`
+    JOIN 00_L2_games_genre ON (00_L2_games.GenreID = 00_L2_games_genre.GenreID)
+    JOIN 00_L2_games_developer ON (00_L2_games.DeveloperID = 00_L2_games_developer.DeveloperID)
+    WHERE `ID` LIKE '$ID'
+    ";
+    
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+
+?>
         <div class="box main">
             <h2>Congratulations</h2>
             
             
             <p>
-               You have added a game to the database.
-            </p>            
+               You have added the following entry
+            </p>      
+            
+            <?php
+            include ("results.php");
+            ?>
 
             
         </div> <!-- / main -->
